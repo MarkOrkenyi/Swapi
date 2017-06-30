e742edcc2f643560e56850040695ef31e1e916be
MarkOrkenyi <mark.orkenyi@gmail.com>
2017-06-30 23:50:34 +0200

    login,registration works, residents modal displays planet's name

--- static/main.js
+++ static/main.js
@@ -14,7 +14,7 @@ function display_planets(planets, next_href, prev_href) {
     for (i = 0; i < planets.length; i++) {
         var residents_nmbr = planets[i].residents.length;
         if (residents_nmbr > 0) {
-            var residents_html = '<td><button class="resident_btn" value="' + planets[i].residents + '">' + residents_nmbr + ' resident(s)</button></td>'
+            var residents_html = '<td><button class="resident_btn" value="' + planets[i].residents + ',' + planets[i].name + '">' + residents_nmbr + ' resident(s)</button></td>'
         } else {
             var residents_html = '<td>No known residents</td>'
         }
@@ -26,11 +26,12 @@ function display_planets(planets, next_href, prev_href) {
     $(".container button#prev_btn").val(prev_href);
     $("button.resident_btn").click(function () {
         var residents_links = ($(this).val()).split(',');
-        residents_modal(residents_links)
+        var planetName = residents_links.pop()
+        residents_modal(residents_links, planetName)
     });
 };
 
-function residents_modal(residents_links) {
+function residents_modal(residents_links, planetName) {
     var residents = [];
     for (i = 0; i < residents_links.length; i++) {
         $.ajax({
@@ -48,9 +49,15 @@ function residents_modal(residents_links) {
     for (i = 0; i < residents.length; i++) {
         residents_table.push('<tr><td>' + residents[i][0] + '</td><td>' + residents[i][1] + '</td><td>' + residents[i][2] + '</td><td>' + residents[i][3] + '</td><td>' + residents[i][4] + '</td><td>' + residents[i][5] + '</td><td>' + residents[i][6] + '</td><td>' + residents[i][7] + '</td></tr>');
     };
+    $("h4.modal-title").text('Residents of ' + planetName);
     $(".table.table-bordered#residents tbody").empty();
     $(".table.table-bordered#residents tbody").append(residents_table);
     $("#residentModal").modal();
+    $(function () {
+        $('.close').click(function () {
+            $('#residentModal').modal('hide');
+        });
+    });
 
 };
 
@@ -61,11 +68,32 @@ $(document).ready(function () {
         prev_href = response['previous'];
         display_planets(planets, next_href, prev_href);
     });
+    $(".container button").click(function () {
+        var api_href = $(this).val();
+        next_table(api_href);
+        return false;
+    });
+    $(function () {
+        if (window.location.href === "http://127.0.0.1:5000/login_user") {
+            var loginUsername = $("meta[name='loginUsername']").attr("content");
+            var loginStatus = $("meta[name='loginStatus']").attr("content");
+            sessionStorage.setItem("loginStatus", loginStatus);
+            sessionStorage.setItem("loginUsername", loginUsername);
+            window.setTimeout(function () { window.location = "/"; }, 1000);
+        };
+        if (window.location.href === "http://127.0.0.1:5000/register_user") {
+            window.setTimeout(function () { window.location = "/"; }, 1000);
+        };
+    });
+    $(function () {
+        if (sessionStorage.getItem("loginUsername") !== null) {
+            $("html body nav.navbar.navbar-default div.container-fluid a#loggedIn").text(sessionStorage.getItem("loginUsername"));
+        };
+    });
+    $(function () {
+        $("html body nav.navbar.navbar-default div.container-fluid ul.nav.navbar-nav li a#logout").click(function () {
+            sessionStorage.clear();
+            window.location = "/";
+        });
+    });
 });
-
-$(".container button").click(function () {
-    var api_href = $(this).val();
-    next_table(api_href);
-    return false;
-});
-
